39eb5a9b8f2248523e8afb473d8e7598d301e5b7
Puneet Gupta <puneetg@microsoft.com>
2025-05-28 16:45:29 -0700

    Fixing build

---
+++ DiagnosticScenarios.Web/Scenarios/HighConnections/HighConnections1.aspx.designer.cs
@@ -0,0 +1,10 @@
+namespace DiagnosticScenarios.Web.Scenarios.HighConnections
+{
+    public partial class HighConnections1
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.Button btnTriggerConnections;
+        protected global::System.Web.UI.WebControls.Label lblStatus;
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl progress;
+    }
+} 
---
+++ DiagnosticScenarios.Web/Scenarios/HighConnections/HighConnections1Actual.aspx.designer.cs
@@ -0,0 +1,8 @@
+namespace DiagnosticScenarios.Web.Scenarios.HighConnections
+{
+    public partial class HighConnections1Actual
+    {
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        protected global::System.Web.UI.WebControls.Label lblStatus;
+    }
+} 
